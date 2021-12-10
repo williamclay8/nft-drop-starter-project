f6c0f9da923266d259fef8cf053872c743361f58
williamclay8 <93858823+williamclay8@users.noreply.github.com>
2021-12-10 11:28:16 -0600

    commit

--- app/src/App.js
+++ app/src/App.js
@@ -15,6 +15,12 @@ const App = () => {
       if (solana) {
         if (solana.isPhantom) {
           console.log('Phantom wallet found!');
+          
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+          );
         }
       } else {
         alert('Solana object not found! Get a Phantom Wallet 👻');
